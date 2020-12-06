50baed1d49a899c88a1b4357209643d659834592
MINGEUN <aviation6890@gmail.com>
2020-12-06 20:18:20 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 
-function Potato(){
-  return <h1>I like potato</h1>;
-}
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!!</h1>
-      <Potato />
-    </div>
+  state = {
+    count : 0,
+  };
+  
+  add = () =>{
+    //this.setState({count : this.state.count +1})
+    this.setState(current =>({
+      count : current.count +1,
+    }))
+
+    console.log('add');
+  }
+
+  minus = () => {
+    //this.setState({count : this.state.count -1});
+    this.setState(current =>({
+      count : current.count -1,
+    }))
     
-  )
-  ;
+    console.log('minus');
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
